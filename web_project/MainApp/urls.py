f59167a7b61bcb80d326508d2d938e2d864529fe
Hassan Mata <ajdourhassan29@gmail.com>
2020-12-15 19:11:03 +0100

    add all chnanges

--- web_project/MainApp/urls.py
+++ web_project/MainApp/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
-
 
 urlpatterns = [
     path('' , views.home , name='home'),
-    path('index/' , views.index , name='index'),
+    path('about/' , views.about , name='about'),
+    path('dashboard/' , views.dashboard , name='dashboard'),
+] 
 
-] + static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
     
--- web_project/MainApp/views.py
+++ web_project/MainApp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here .
 
 def home(request) :
     return render(request , 'home.html')
 
-def index(request) :
-    return render(request , 'index.html')
+def about(request) :
+    return render(request, 'about.html')
+
+def dashboard(request) :
+    return render(request , 'dashboard.html')
+
