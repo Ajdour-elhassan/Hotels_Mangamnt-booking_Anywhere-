30f0f2446a923f2120492aad48e9d3c2c51e78c9
Hassan Mata <ajdourhassan29@gmail.com>
2020-12-17 17:42:27 +0100

    add all changes

--- web_project/web_project/urls.py
+++ web_project/web_project/urls.py
@@ -21,7 +21,10 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('MainApp.urls')),
+    path('lists/' , include('lists.urls')),
+    #path('realts/' , include('realts.urls')),
 ]
+
 if settings.DEBUG :
     urlpatterns += static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
